97daf576f0de0cd808d06bce45ccf43a62ab6de0
jihoon-devstu <wlsgksvheh@gmail.com>
2026-06-29 14:08:55 +0900

    Feat: Spring Security 첫 설정 및 application.yml 셋팅추가

---
+++ src/main/java/com/paw/ddasoom/common/security/CorsProperties.java
@@ -0,0 +1,15 @@
+package com.paw.ddasoom.common.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app.cors")  // "cors"로 시작하는 프로퍼티 바인딩
+public class CorsProperties {
+  private String allowedOrigins;
+}
