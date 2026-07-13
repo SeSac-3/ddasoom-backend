2a517fe0ea64f4de19629b5427a6e37db0f7a7ef
seojin <seojointly@gmail.com>
2026-07-13 22:16:34 +0900

    🐛 fix: BaseTimeEntity java code 추가 변경

--- src/main/java/com/paw/ddasoom/common/util/BaseTimeEntity.java
+++ src/main/java/com/paw/ddasoom/common/util/BaseTimeEntity.java
@@ -2,26 +2,22 @@ package com.paw.ddasoom.common.util;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.Generated;
+import org.hibernate.generator.EventType;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class) // JPA Auditing 이벤트 리스너 활성화
 public abstract class BaseTimeEntity {
 
-  @CreatedDate
-  @Column(updatable = false, nullable = false,  columnDefinition = "DATETIME(6)")
+  @Generated(event = EventType.INSERT)
+  @Column(insertable = false, updatable = false, nullable = false, columnDefinition = "DATETIME(6)")
   private LocalDateTime createdAt;
 
-  @LastModifiedDate
-  @Column(nullable = false ,  columnDefinition = "DATETIME(6)")
+  @Generated(event = { EventType.INSERT, EventType.UPDATE })
+  @Column(insertable = false, updatable = false, nullable = false, columnDefinition = "DATETIME(6)")
   private LocalDateTime updatedAt;
-
 }
